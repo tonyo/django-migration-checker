68e9bd3f5f0534f055a82ea4b37eaf18775c7a8e
Anton Ovchinnikov <revolver112@gmail.com>
2016-09-11 01:29:33 +0200

    Bump version: 0.1.0 → 0.2.0

--- django_migration_checker/__init__.py
+++ django_migration_checker/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Anton Ovchinnikov'
 __email__ = 'anton.ovchi2nikov@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='django_migration_checker',
-    version='0.1.0',
+    version='0.2.0',
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     long_description=readme + '\n\n' + history,
     author="Anton Ovchinnikov",
