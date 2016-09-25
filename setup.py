806adc417547811d33980979cadaffa552876952
Anton Ovchinnikov <revolver112@gmail.com>
2016-09-25 15:27:16 +0200

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test_requirements = [
 
 setup(
     name='django-migration-checker',
-    version='0.3.0',
+    version='0.4.0',
     description="Static migration conflict checker for Django",
     long_description=readme + '\n\n' + history,
     author="Anton Ovchinnikov",
