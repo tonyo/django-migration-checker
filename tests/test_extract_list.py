cb296d774ccd634a5be2bc78632b15597a53339c
Falldog <falldog@hardcoretech.co>
2022-07-28 03:39:06 +0800

    Fix error on double quote and multiple dependencies (#11)
    - update the reg exp about the ending double quote
    - add unit-test for extract-list

---
+++ tests/test_extract_list.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import sys
+import unittest
+
+from django_migration_checker.base import extract_list
+
+
+class TestExtractList(unittest.TestCase):
+    def test_basic_single_quote(self):
+        content = """
+            dependencies = [
+                ('llamas', '0001_one'),
+            ]
+        """
+        assert [('llamas', '0001_one')] == extract_list('dependencies', content)
+
+    def test_basic_double_quote(self):
+        content = """
+            dependencies = [
+                ("llamas", "0001_one"),
+            ]
+        """
+        assert [('llamas', '0001_one')] == extract_list('dependencies', content)
+
+    def test_basic_compact_style(self):
+        content = """
+            dependencies=[('llamas','0001_one')]
+        """
+        assert [('llamas', '0001_one')] == extract_list('dependencies', content)
+
+    def test_multi_dependencies_single_quote(self):
+        content = """
+            dependencies = [
+                ('llamas', '0001_one'),
+                ('llamas', '0002_two'),
+                ('llamas', '0003_three'),
+            ]
+        """
+        assert [
+                   ('llamas', '0001_one'),
+                   ('llamas', '0002_two'),
+                   ('llamas', '0003_three'),
+               ] == extract_list('dependencies', content)
+
+    def test_multi_dependencies_double_quote(self):
+        content = """
+            dependencies = [
+                ("llamas", "0001_one"),
+                ("llamas", "0002_two"),
+                ("llamas", "0003_three"),
+            ]
+        """
+        assert [
+                   ('llamas', '0001_one'),
+                   ('llamas', '0002_two'),
+                   ('llamas', '0003_three'),
+               ] == extract_list('dependencies', content)
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
