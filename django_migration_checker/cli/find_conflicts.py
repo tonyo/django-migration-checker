458f09dfb7f8b3fc54b2566aa6ee1eea3c60380a
Jonas Vacek <jvacek@users.noreply.github.com>
2022-12-11 16:13:31 +0100

    feat: Handle multiple projects (#12)

--- django_migration_checker/cli/find_conflicts.py
+++ django_migration_checker/cli/find_conflicts.py
@@ -7,19 +7,30 @@ from django_migration_checker import get_conflicts
 
 def create_parser():
     parser = argparse.ArgumentParser(
-        description='Find migration conflicts in Django projects.'
+        description="Find migration conflicts in Django projects."
     )
-    parser.add_argument('apps_dir', metavar='APPS_DIR',
-                        help='directory with Django applications')
+    parser.add_argument(
+        "apps_dirs",
+        metavar="APPS_DIRS",
+        nargs="+",
+        help="one or more directory with Django applications",
+    )
     return parser
 
 
 def main():
     parser = create_parser()
     args = parser.parse_args()
-    conflicts = get_conflicts(args.apps_dir)
-    if conflicts:
-        print(conflicts)
+    exit_1 = False
+    for dir in args.apps_dirs:
+        if len(args.apps_dirs) > 1:
+            print(dir + ":")
+            
+        conflicts = get_conflicts(dir)
+        if conflicts:
+            print(conflicts)
+            exit_1 = True
+        else:
+            print("No conflicts detected.")
+    if exit_1:
         sys.exit(1)
-    else:
-        print('No conflicts detected.')
